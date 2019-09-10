f628ead1a0b065db317cf381c29edff6d442ec8c
@slovacus <slovacus@gmail.com>
2019-09-09 20:54:38 -0500

    (#1) test: implement fixture for test

---
+++ conftest.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from pyfacebook.fixtures.core import *  # noqa pylint: disable=W0614,W0401
+from pyfacebook.fixtures.db import *  # noqa pylint: disable=W0614,W0401
+from pyfacebook.fixtures.data import *  # noqa pylint: disable=W0614,W0401
